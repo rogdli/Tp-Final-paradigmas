856a3d8fba6e341f7454b6992160e485917a438e
Rodrigo Villegas <128648211+rogdli@users.noreply.github.com>
2023-11-29 00:01:27 -0300

    Add files via upload
    Funciones de Tarea.js sin bucles

--- Tarea.js
+++ Tarea.js
@@ -28,41 +28,74 @@ function crearTarea(titulo, descripcion, vencimiento, dificultad) {
 ///esta funcion devuelve un objeto tipo tarea y pide los valores que completaran al nuevo objeto tipo tarea 
 function ingresar() {
     let titulo, descripcion, vencimiento, dificultad;
+
     titulo = readline.question("Titulo: \n");
     descripcion = readline.question("Descripcion: \n");
-    do {
-        const fechaTexto = readline.question('Ingresa fecha de Vencimiento (YYYY-MM-DD):\n');
+
+    function ingresarFechaVencimiento() {
+        const fechaTexto = readline.question('Ingresa fecha de vencimiento (YYYY-MM-DD):\n');
         vencimiento = new Date(fechaTexto);
-    } while (isNaN(vencimiento.getTime()));
-    while (dificultad != 1 && dificultad != 2 && dificultad != 3) {
-        ///utilizo un console.log en vez de poner la descripcion en el questionInt, porque si no no muestra los emojis
+
+        if (isNaN(vencimiento.getTime())) {
+            console.log('Fecha no válida. Por favor, ingresa una fecha en el formato correcto.');
+            return ingresarFechaVencimiento();
+        }
+
+        return vencimiento;
+    }
+
+    vencimiento = ingresarFechaVencimiento();
+
+    function ingresarDificultad() {
         console.log(`${kleur.green(`1.Facil ${emoji.get('smile')}`)}\n${kleur.yellow(`2.Medio ${emoji.get('neutral_face')}`)}\n${kleur.red(`3.Dificil ${emoji.get('rage')}`)}`);
         dificultad = readline.questionInt();
+
+        if (![1, 2, 3].includes(dificultad)) {
+            console.log('Opción no válida. Por favor, elige una dificultad válida.');
+            return ingresarDificultad();
+        }
+
+        return dificultad;
     }
-    return crearTarea(titulo, descripcion, vencimiento, dificultad);
+
+    return crearTarea(titulo, descripcion, vencimiento, ingresarDificultad());
 }
 
 function editar(x) {
-    x.titulo = readline.question("Titulo: \n");
-    x.descripcion = readline.question("Descripcion: \n");
-    x.estado=5;
-    while (x.estado != 1 && x.estado != 2 && x.estado != 3) {
-        ///utilizo un console.log en vez de poner la descripcion en el questionInt, porque si no no muestra los emojis
-        console.log(`${kleur.red(`1. Pendiente ${emoji.get('tada')}`)}\n${ kleur.yellow(`2. En curso  ${emoji.get('hourglass')}`)}\n${kleur.green(`3. Finalizado ${emoji.get('white_check_mark')}`)}`);
-        x.estado = readline.questionInt();
+    function leerTitulo() {
+        x.titulo = readline.question("Titulo: \n");
+        leerDescripcion();
     }
-    x.ultima_edicion = new Date();
-    do {
+
+    function leerDescripcion() {
+        x.descripcion = readline.question("Descripcion: \n");
+        x.estado = leerEstado();
+    }
+
+    function leerEstado() {
+        console.log(`${kleur.red(`1. Pendiente ${emoji.get('tada')}`)}\n${kleur.yellow(`2. En curso  ${emoji.get('hourglass')}`)}\n${kleur.green(`3. Finalizado ${emoji.get('white_check_mark')}`)}`);
+        const estado = readline.questionInt();
+        return (estado === 1 || estado === 2 || estado === 3) ? estado : leerEstado();
+    }
+
+    function leerFecha() {
         const fechaTexto = readline.question('Ingresa fecha limite (YYYY-MM-DD): ');
-        x.vencimiento = new Date(fechaTexto);
-    } while (isNaN(x.vencimiento.getTime()));
-    ///utilizo un console.log en vez de poner la descripcion en el questionInt, porque si no no muestra los emojis
-    x.dificultad=5;
-    while (x.dificultad != 1 && x.dificultad != 2 && x.dificultad != 3) {
-        ///utilizo un console.log en vez de poner la descripcion en el questionInt, porque si no no muestra los emojis
-        console.log(`${kleur.green(`1.Facil ${emoji.get('smile')}`)}\n${kleur.yellow(`2.Medio ${emoji.get('neutral_face')}`)}\n${kleur.red(`3.Dificil ${emoji.get('rage')}`)}`);
-        x.dificultad = readline.questionInt();
+        const fecha = new Date(fechaTexto);
+        return isNaN(fecha.getTime()) ? leerFecha() : fecha;
     }
+
+    function leerDificultad() {
+        console.log(`${kleur.green(`1. Facil ${emoji.get('smile')}`)}\n${kleur.yellow(`2. Medio ${emoji.get('neutral_face')}`)}\n${kleur.red(`3. Dificil ${emoji.get('rage')}`)}`);
+        const dificultad = readline.questionInt();
+        return (dificultad === 1 || dificultad === 2 || dificultad === 3) ? dificultad : leerDificultad();
+    }
+
+    x.descripcion = leerTitulo();
+    x.estado = leerEstado();
+    x.ultima_edicion = new Date();
+    x.vencimiento = leerFecha();
+    x.dificultad = leerDificultad();
+
     console.log("Datos guardados!\n");
 }
 
